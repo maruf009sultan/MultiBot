bf588e5afa1e61b84fbef2e7ed27a48d984668be
maruf009sultan <maruf009sultan@gmail.com>
2025-08-02 00:23:47 +0600

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for bot in $(cat config.json | jq -r 'to_entries[] | "\(.key),\(.value.source)"'); do
+  name=$(echo $bot | cut -d',' -f1)
+  source=$(echo $bot | cut -d',' -f2)
+  git clone $source $name
+  cd $name && pip install --no-cache-dir -r requirements.txt && cd ..
+done
